96205d8d074b57c30cead32f7c8dfbb8146934fb
PilowXe <78667474+alexander695@users.noreply.github.com>
2022-04-29 17:59:38 -0700

    Update IP.py

--- IP.py
+++ IP.py
@@ -1,5 +1,7 @@
 import os 
 from colorama import Fore 
+import webbrowser
+import time
 
 #banner#
 def menu():
@@ -24,27 +26,24 @@ while True:
  if opcionMenu=="1":
      if opcionMenu=="1":
          print ("starting ngrok")
-         os.system("cp -a site/index.html. /var/www/html/index.html")
-         os.system("sudo service apache2 start")
+         os.system("cp -a site/index.html /var/www/html/index.html")
          os.system("chmod 777 ngrok")
-         os.system("./ngrok authtoken 1otl8ouPfwH3JwCDCD4fxu2YCgq_624ab5GeqzJhPPwHUbauT")
          os.system("xterm -hold -e ./ngrok http 8080")
          webbrowser.open("http://127.0.0.1:4040", new=2, autoraise=True)
+         print("If you don't have any idea of ngrok go to https://github.com/alexander695/IPloger")
+         print("If web browser don't, open and go to http://127.0.0.1:4040")
          input("\npress enter to back")
          os.system("clear")
      
- else:
-     print("A problem was ocurred, ngrok was installed?")       
-     os.system("clear")
 
  if opcionMenu=="2":
      os.system("sudo apt-get install unzip -y")
      os.system("wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip")
      os.system("unzip ngrok-stable-linux-amd64.zip")
-     input("\npress enter to back")
-     os.system("clear")
+     print("Ngrok was installed")
+     time.sleep(3)
  
  else:
-     print("an error was ocurred with the installation :c")
-     os.system("clear")
+     input("An error was ocurred press enter to back")
+
 
